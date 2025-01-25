1cde6f78f5c4980ace8cf43458b10f35afed2a53
bajajku <bajajku@sheridancollege.ca>
2025-01-25 18:43:47 -0500

    Add JPA support with H2 database and create User model
    - Update Spring Boot version to 3.2.3
    - Add Spring Data JPA and H2 database dependencies
    - Remove Maven pom.xml
    - Create User entity implementing UserDetails
    - Configure H2 in-memory database in application.properties

---
+++ src/main/java/com/example/petcare/model/User.java
@@ -0,0 +1,68 @@
+package com.example.petcare.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.lang.annotation.Inherited;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+    private Long id;
+    @Column(unique = true, nullable = false)
+
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+
+    // Constructor for creating an unverified user
+
+    public User(String username, String email, String password, String role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    // Default constructor
+    public User(){}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
