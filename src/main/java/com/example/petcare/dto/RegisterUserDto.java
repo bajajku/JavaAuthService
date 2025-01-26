b3d39c150042f002315423e1655d0234332b4c5a
bajajku <bajajku@sheridancollege.ca>
2025-01-26 01:35:46 -0500

    Added dtos

---
+++ src/main/java/com/example/petcare/dto/RegisterUserDto.java
@@ -0,0 +1,11 @@
+package com.example.petcare.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String username;
+}
