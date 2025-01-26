8d4c8aee55281e368bbb8cc688e1ce6e31a77e49
bajajku <bajajku@sheridancollege.ca>
2025-01-26 01:54:12 -0500

    Added userService and Controller.

---
+++ src/main/java/com/example/petcare/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.petcare.service;
+
+import com.example.petcare.model.User;
+import com.example.petcare.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository, EmailService emailService) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
